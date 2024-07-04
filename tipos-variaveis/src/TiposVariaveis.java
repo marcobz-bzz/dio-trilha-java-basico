2812c9a1fb44687a81c453e3416d5746bbd96443
marcobz-bzz <marcobz@gmail.com>
2024-07-04 00:32:17 -0300

    feat: projeto inicial java

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,10 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        System.out.println("argum = " + args.length);
+
+        for(int ii=0;ii<args.length;ii++){
+            System.out.println("argumento ["+ii+"] = "+args[ii]);
+        }
     }
 }
