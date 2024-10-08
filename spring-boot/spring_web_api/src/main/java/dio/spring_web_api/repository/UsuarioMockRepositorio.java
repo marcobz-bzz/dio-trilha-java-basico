8ebf0ec28f1b058bab844aced3340c8c15d5ed93
marcobz-bzz <marcobz@gmail.com>
2024-10-07 21:53:13 -0300

    web api repositorio mock

--- spring-boot/spring_web_api/src/main/java/dio/spring_web_api/repository/UsuarioMockRepositorio.java
+++ spring-boot/spring_web_api/src/main/java/dio/spring_web_api/repository/UsuarioMockRepositorio.java
@@ -1,8 +1,44 @@
 package dio.spring_web_api.repository;
 
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
+
+import dio.spring_web_api.model.Usuario;
 
 @Repository
 public class UsuarioMockRepositorio {
+    public void save(Usuario usuario) {
+        if (usuario.getIdUsuario() == null) {
+            System.out.println(">>> salvando usuario: " + usuario.getNome());
+        } else {
+            System.out.println(">>> atualizando usuario: " + usuario.getNome());
+        }
+    }
 
+    public void deleteById(Integer idUsuario) {
+        System.out.println(">>> deletando usuario com id = " + idUsuario);
+    }
+
+    public List<Usuario> findAll() {
+        Usuario u1 = new Usuario("bzz", "123");
+        u1.setIdUsuario(1);
+        Usuario u2 = new Usuario("fbz", "456");
+        u2.setIdUsuario(2);
+        Usuario u3 = new Usuario("mcbz", "789");
+        u3.setIdUsuario(3);
+        return List.of(u1, u2, u3);
+    }
+
+    public Usuario findById(Integer idUsuario) {
+        Usuario uu = new Usuario("bzz", "123");
+        uu.setIdUsuario(idUsuario);
+        return uu;
+    }
+
+    public Usuario findByUserName(String userName) {
+        Usuario uu = new Usuario(userName, "123");
+        uu.setIdUsuario(99);
+        return uu;
+    }
 }
